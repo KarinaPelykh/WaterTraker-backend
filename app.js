9ce70f62152f63ccc2cb08a5d286afc6ec361cbc
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-10-28 18:38:05 +0100

    add: structure for project & server file split on two parts

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const moment = require("moment");
+
+const fs = require("fs/promises");
+
+const authRouter = require("./src/routes/api/auth");
+const userRouter = require("./src/routes/api/user");
+
+const app = express();
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  const { method, url } = req;
+  const data = moment().format("DD-MM-YY_hh:mm:ss");
+
+  await fs.appendFile("./server.log", `\n${method} ${url} ${data}`);
+  next();
+});
+
+app.use((req, res) => {
+  res.status(404).json({ massage: "Not found" });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
+const app = require("./app");
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("server is running");
-});
-
-app.use(cors());
-
-app.use((req, res, next) => {
-  console.log("middleware");
-
-  next();
-});
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.json());
+app.listen(port);
