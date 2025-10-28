a2b9d76213724540318d2f3539b0720de02b4696
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-10-28 19:03:15 +0100

    add: HttpError & Joi

---
+++ src/helpers/HttpError.js
@@ -0,0 +1,12 @@
+const errorStatus = {
+  400: "Bad request",
+  401: "Unauthorized",
+  404: "Not found",
+  500: "Server error",
+};
+
+const HttpError = (status) => {
+  const error = new Error(errorStatus[status]);
+  error.status = status;
+  return error;
+};
