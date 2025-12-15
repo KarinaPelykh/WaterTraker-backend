1c66199bf46f82f56e93a21fb5eee481969dd1a8
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-12-16 00:57:13 +0100

    fix: auth schema

--- src/schemas/users.js
+++ src/schemas/users.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 const emailRegexp = require("../constants/user-constants");
 
 const userSignupSchema = Joi.object({
-  name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
